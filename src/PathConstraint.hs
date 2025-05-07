095dc0d45319a85a25015e11f23a5747c08ed103
Shiva Tamil Kumaran <shivatk01@gmail.com>
2025-05-07 14:18:11 +0100

    Restarting

--- src/PathConstraint.hs
+++
@@ -1,9 +0,0 @@
-module PathConstraint where
-
-import           Assertion         (Assertion (Pure))
-import           LogicalExpression (LogicalExpression)
-
-type PathConstraint = [LogicalExpression]
-
-assertions :: PathConstraint -> [Assertion p]
-assertions = map Pure
--- src/SymbolicMemory.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE FunctionalDependencies #-}
-
-module SymbolicMemory where
-
-import Assertion (Assertion)
-
-
-class SymbolicMemory m p | m -> p where
-  assertions :: m -> [Assertion p]
